1912776fd88120e319375ff71e0e6c397e8ba2aa
Noor Zaqir <nzscode@gmail.com>
2023-11-10 12:31:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from views import views
 
 app = Flask (__name__)
+
+# THe route has been set up in the views.py instead.
+#
+# @app.route("/")
+# def home():
+#     return "This is the home page"
+#
+
+app.register_blueprint(views, url_prefix='/views/lala')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
